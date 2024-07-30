8a3eb86e849046e7e52ede652fbafd226af0747a
Andrew West <alwest@mgh.harvard.edu>
2024-07-30 14:48:41 -0400

    firebase updates for loading data

--- lib/application/repository.dart
+++ lib/application/repository.dart
@@ -4,18 +4,31 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:intl/intl.dart';
 import 'package:mghheartaccess/application/database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:mghheartaccess/application/utils.dart';
 import 'package:mghheartaccess/model/exception.dart';
 import 'package:mghheartaccess/model/heart_center.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Repository {
   late MGHHeartDatabase _database;
+  late final SharedPreferences prefs;
 
   // this is the data structure that holds dynamic data
   HeartCenter? _heartCenter;
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localDataFile async {
+    final path = await _localPath;
+    return File('$path/appData.json');
+  }
 
   Repository() {
     init();
@@ -24,11 +37,29 @@ class Repository {
   Future init() async {
     print('Repository: init ...');
 
+    prefs = await SharedPreferences.getInstance();
+
+    DateTime? lastUpdated;
+    String? lastUpdatedString = prefs.getString('lastUpdated');
+    DateFormat df = DateFormat('yyyy/MM/dd HH:mm');
+
+    if (lastUpdatedString != null) {
+      lastUpdated = df.parse(lastUpdatedString);
+    }
+
+    print('Repository: lcoal last updated date = $lastUpdated');
+
+    DateTime? fbLastUpdated = await getLastUpdatedFromFirebase();
+
+    // look for document in docs directory
+
     // if def file in documents directlry is null, load from assets directory
 
     // if def file exists in documents directory load it to get version
 
     // get version info from firebase
+
+    // get last update date from app properties
 
     // if new version is available load from firebase
     // else just use current def file
@@ -37,6 +68,21 @@ class Repository {
     await _database.init();
 
     print('Repository: init ... done');
+  }
+
+  Future<DateTime?> getLastUpdatedFromFirebase() async {
+    final storageRef = FirebaseStorage.instance.ref();
+
+    // Create a reference with an initial file path and name
+    final pathReference = storageRef.child("test.json");
+
+    FullMetadata fullMetadata = await pathReference.getMetadata();
+
+    DateTime? lastUpdated = fullMetadata.updated;
+
+    print('Repository: last updated for data file = $lastUpdated');
+
+    return lastUpdated;
   }
 
   Future<Map<String, dynamic>> loadJSONFromFireabse() async {
